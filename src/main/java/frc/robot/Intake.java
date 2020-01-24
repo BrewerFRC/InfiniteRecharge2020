5721dc4cb18f26c22c792318d0bd1411a6b21664
Zyntauro <einstein3203@gmail.com>
2020-01-24 18:55:16 -0500

    the classes intake magazine  flywheel shooter.

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,53 @@
+package frc.robot;
+
+public class Intake {
+
+    /**
+     * 
+     */
+    public void init(){
+
+    }
+
+    /**
+     * 
+     */
+	public void debug(){
+
+    }
+
+    /**
+     * 
+     */
+	public void posIntake(){
+
+    }
+
+    /**
+     * 
+     */
+	public void stopIntake(){
+
+    }
+
+    /**
+     * 
+     */
+	public void negIntake(){
+
+    }
+
+    /**
+     * 
+     */
+	public void setIntakeUp(){
+
+    }
+
+    /**
+     * 
+     */
+	public void setIntakeDown() {
+
+    }
+}
---
+++ src/main/java/frc/robot/Magazine.java
@@ -0,0 +1,74 @@
+package frc.robot;
+
+public class Magazine {
+
+    /**
+     * 
+     */
+    public void init(){
+
+    }
+
+    /**
+     * 
+     */
+	public void debug(){
+
+    }
+
+    /**
+     * 
+     */
+	public boolean bottomSensorTriggered(){
+
+    }
+
+    /**
+     * 
+     */
+	public boolean topSensorTriggered(){
+
+    }
+
+    /**
+     * 
+     */
+	public boolean fullyLoaded(){
+
+    }
+
+    /**
+     * 
+     */
+	public void load(){
+
+    }
+
+    /**
+     * 
+     */
+	public void stop(){
+
+    }
+
+    /**
+     * 
+     */
+	public void unload(){
+
+    }
+
+    /**
+     * 
+     */
+	public void shootBall(){
+
+    }
+
+    /**
+     * 
+     */
+	public void LoadBreach(){
+
+    }
+}
