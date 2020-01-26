4fab7cb3c4d0f2598f33b30001a4b5e6ab519316
Zyntauro <einstein3203@gmail.com>
2020-01-26 10:48:28 -0500

    added countdown may or may not be used and doing some testing in robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,13 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 
 class Robot extends TimedRobot {
   DriveTrain dt = new DriveTrain();
   Xbox driver = new Xbox(0);
   Xbox operator =  new Xbox(1);
+  Timer timer = new Timer();
 
   
   @Override
@@ -19,6 +21,7 @@ class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
+    Common.dashBool("time passed", timer.hasPeriodPassed(5));
   }
 
 
@@ -36,6 +39,10 @@ class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     dt.accelDrive(driver.deadzone(driver.getX(GenericHID.Hand.kLeft)), driver.deadzone(driver.getY(Hand.kRight)));
+    if (driver.getPressed(Xbox.buttons.x)) {
+    timer.reset();
+    timer.start();
+    }
     
     
     debug();
