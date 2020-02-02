4f8aff8056483cd64a6ca1b77e0e126b262b492f
BrentRoberts989 <prog@chaos.local>
2020-02-02 16:59:04 -0500

    Updated constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,8 +9,7 @@ package frc.robot;
  */
 public class Constants {
     //PWM
-    static final int PWM_INTAKE_MOTOR = 0;
-    static final int PWM_COLORWHEEL_MOTOR = 1;
+    static final int PWM_INTAKE_MOTOR = 0, PWM_COLORWHEEL_MOTOR = 1, PWM_MAGAZINE_MOTOR = 2;
 
     //DIO
     public static final int DIO_TOP_BEAMBREAK = 0, DIO_BOTTOM_BEAMBREAK = 1;
@@ -23,7 +22,8 @@ public class Constants {
     
 
     //Motors
-    public static final int DRIVE_FL = 13, DRIVE_ML = 14, DRIVE_BL =15, DRIVE_FR = 11, DRIVE_MR =10, DRIVE_BR = 12, CAN_MAGAZINE_MOTOR = 16, FLYWHEEL_LEFT_CAN_ID = 4, FLYWHEEL_RIGHT_CAN_ID = 5;
+    public static final int DRIVE_FL = 13, DRIVE_ML = 14, DRIVE_BL =15, DRIVE_FR = 11, DRIVE_MR = 10, DRIVE_BR = 12,
+    FLYWHEEL_LEFT_CAN_ID = 16, FLYWHEEL_RIGHT_CAN_ID = 17;
 
     //Devices
     public static final int PCM_CAN_ID = 2;
